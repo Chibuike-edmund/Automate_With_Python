3397783aa043d949cf79eceb9609f06237a1c412
Precious Edmund <Chidera1915@gmail.com>
2024-05-10 17:35:48 +0000

    update

--- Project #1/Table Extraction.py
+++ Project #1/Table Extraction.py
@@ -1,6 +1,12 @@
+from PyPDF2 import PdfReader
 import camelot
 
-tables = camelot.read_pdf('foo.df', pages='1')
+pdf = PdfReader('/workspaces/Automate_With_Python/Project #1/foo.pdf')
 
+tables = camelot.read_pdf('/workspaces/Automate_With_Python/Project #1/foo.pdf', pages='1')
 
 print(tables)
+
+
+tables.export('foo.csv', f='csv', compress=True)
+tables[0].to_csv('foo.csv')
