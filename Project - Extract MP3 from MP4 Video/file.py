e70b877a332be6f94cf3a108bf70be0ff55f7327
Chibuike-edmund <chidera1915@gmail.com>
2024-06-24 16:37:49 +0000

    update

---
+++ Project - Extract MP3 from MP4 Video/file.py
@@ -0,0 +1,10 @@
+import ffmpeg
+
+
+input_file = 'input.mp4'
+output_file = 'output.mp3'
+
+# Extract audio from the video
+ffmpeg.input(input_file).output(output_file, format='mp3').run()
+
+print('Audio extracted successfully!')
